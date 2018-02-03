c3f5599ad184f266b9ce4a436bf2741d95743379
Maor Leger <maor.leger@gmail.com>
2018-02-02 17:14:54 -0700

    install vim 8

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ set -x -e
 
 # Install dependencies
 sudo add-apt-repository ppa:neovim-ppa/stable -y
+sudo add-apt-repository ppa:jonathonf/vim -y
 sudo apt-get update
 sudo apt-get install vim neovim tmux git python unzip -y
 
